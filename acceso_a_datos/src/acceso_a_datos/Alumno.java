f226061554e9a1714e8a6feb4572a9b8677f34f5
alumno <alumno@DAM206>
2025-09-25 12:43:31 +0200

    Tarea 1

---
+++ acceso_a_datos/src/acceso_a_datos/Alumno.java
@@ -0,0 +1,93 @@
+package acceso_a_datos;
+
+import java.time.LocalDate;
+//Tendria que 
+public class Alumno {
+	private int nia;
+	private String nombre,apellidos,ciclo,curso,grupo;
+	private char genero;
+	private LocalDate fecha;
+	
+	static int acumulador=1;
+	
+	public Alumno() {
+		this.nia = acumulador;
+		this.nombre ="nombre"+acumulador;
+		this.apellidos = "apellidos"+acumulador;
+		this.ciclo = "ciclo"+acumulador;
+		this.curso = "curso"+acumulador;
+		this.grupo = "grupo"+acumulador;
+		this.genero =(acumulador%2==0)?'M':'F' ;
+		this.fecha = LocalDate.now().plusMonths(acumulador);
+		acumulador++;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alumno [nia=" + nia + ", nombre=" + nombre + ", apellidos=" + apellidos + ", ciclo=" + ciclo
+				+ ", curso=" + curso + ", grupo=" + grupo + ", genero=" + genero + ", fecha=" + fecha + "]";
+	}
+
+	public Alumno(int nia, String nombre, String apellidos, String ciclo, String curso, String grupo, char genero,
+			LocalDate fecha) {
+		
+		this.nia = nia;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.ciclo = ciclo;
+		this.curso = curso;
+		this.grupo = grupo;
+		this.genero = genero;
+		this.fecha = fecha;
+	}
+	
+	public int getNia() {
+		return nia;
+	}
+	public void setNia(int nia) {
+		this.nia = nia;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getCiclo() {
+		return ciclo;
+	}
+	public void setCiclo(String ciclo) {
+		this.ciclo = ciclo;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getGrupo() {
+		return grupo;
+	}
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+	public char getGenero() {
+		return genero;
+	}
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+}
+
--- acceso_a_datos/src/acceso_a_datos/Main.java
+++ acceso_a_datos/src/acceso_a_datos/Main.java
@@ -1,10 +1,176 @@
 package acceso_a_datos;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+	public static Scanner prompt = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Alumno> alumnos = new ArrayList<>();
+		System.out.println("Mostrar alumnos:");
+		for (int i = 0; i < 10; i++) {
+			Alumno alumno = new Alumno();
+			alumnos.add(alumno);
 
+			System.out.println(alumno.toString() + "\n");
+		}
+
+		System.out.println("Mostrar alumnos ordenados por Nia:");
+		alumnos.sort(Comparator.comparing(Alumno::getNia));
+
+		for (Alumno alumno : alumnos) {
+			System.out.println(alumno.toString() + "\n");
+		}
+		
+		menu(alumnos);
+
+	}
+
+	public static void menu(List<Alumno> alumnos) {
+		
+		int opcion=0;
+		
+		do {
+			System.out.println("Elige una opcion: 1-Añadir Alumno. 2-Mostrar Alumnos ordenados. 3-Salir.");
+			opcion=prompt.nextInt();
+			
+			switch(opcion) {
+			case 1:
+				
+				anadirAlumno(alumnos);
+				break;
+			case 2:
+				
+				mostrarAlumnos(alumnos);
+				break;
+			
+			case 3:
+				System.out.println("fin del programa");
+				break;
+			
+			}
+				
+		}while(opcion!=3);
+	}
+
+	public static void mostrarAlumnos(List<Alumno> alumnos) {
+		int opcion = 0;
+		System.out.println(
+				"como quieres ordenar los alumnos: 1-Nia. 2-Ciclo. 3-Curso. 4-Grupo. 5-Nombre. 6-Apellidos. 7-Fecha. 8-Genero.");
+			opcion=prompt.nextInt();
+		switch (opcion) {
+		case 1:
+			System.out.println("Mostrar alumnos ordenados por Nia:");
+			alumnos.sort(Comparator.comparing(Alumno::getNia));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+
+		case 2:
+			System.out.println("Mostrar alumnos ordenados por Ciclo:");
+			alumnos.sort(Comparator.comparing(Alumno::getCiclo));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+
+			break;
+
+		case 3:
+			System.out.println("Mostrar alumnos ordenados por Curso:");
+			alumnos.sort(Comparator.comparing(Alumno::getCurso));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+
+		case 4:
+			System.out.println("Mostrar alumnos ordenados por Grupo:");
+			alumnos.sort(Comparator.comparing(Alumno::getGrupo));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+		case 5:
+			System.out.println("Mostrar alumnos ordenados por Nombre:");
+			alumnos.sort(Comparator.comparing(Alumno::getNombre));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+		case 6:
+			System.out.println("Mostrar alumnos ordenados por Apellidos:");
+			alumnos.sort(Comparator.comparing(Alumno::getApellidos));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+		case 7:
+			System.out.println("Mostrar alumnos ordenados por Fecha:");
+			alumnos.sort(Comparator.comparing(Alumno::getGenero));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+			
+		case 8:
+			System.out.println("Mostrar alumnos ordenados por genero:");
+			alumnos.sort(Comparator.comparing(Alumno::getGenero));
+
+			for (Alumno alumno : alumnos) {
+				System.out.println(alumno.toString() + "\n");
+			}
+			break;
+
+		default:
+			System.out.println("Fuera de rango.");
+		}
+
+	}
+
+	public static void anadirAlumno(List<Alumno> alumnos) {
+		System.out.println("Introduce el Nia:");
+		int nia = prompt.nextInt();
+
+		System.out.println("Introduce el nombre:");
+		String nombre = prompt.nextLine();
+
+		System.out.println("Introduce el apellidos:");
+		String apellidos = prompt.nextLine();
+
+		System.out.println("Introduce el genero:1-M 2-F");
+		char genero = (prompt.nextInt() == 1) ? 'M' : 'F';
+
+		System.out.println("Introduce el curso:");
+		String curso = prompt.nextLine();
+
+		System.out.println("Introduce el ciclo:");
+		String ciclo = prompt.nextLine();
+
+		System.out.println("Introduce el grupo:");
+		String grupo = prompt.nextLine();
+
+		System.out.println("Introduce la fecha (formato-> xx/xx/xxxx):");
+		String fecha = prompt.nextLine();
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/mm/yyyy");
+
+		alumnos.add(new Alumno(nia, nombre, apellidos, ciclo, curso, grupo, genero, LocalDate.parse(fecha, formatter)));
+		System.out.println("Alumno añadido correctamente.");
 	}
 
 }
