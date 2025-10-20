cdcd46596082f69e84be50e7a465f3d45196f877
alumno alumno@DAM206 <juanmezcua96@gmail.com>
2025-10-20 11:48:49 +0200

    ejemplo para ver variables son punteros de objeto

---
+++ acceso_a_datos/src/ejemplos/Punteros.java
@@ -0,0 +1,14 @@
+package ejemplos;
+
+public class Punteros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=new String("hola");
+		String s2=s1;
+		s2.concat("mundo");
+		
+		System.out.println(s1);
+	}
+
+}
