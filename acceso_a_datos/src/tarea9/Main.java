96659fb1e7b3349d59897d0d0841e14d7ea2eba6
juan-mezcla <juanmezcua96@gmail.com>
2025-11-03 13:39:25 +0100

    Tarea9 apartado 1 añadido a clase, solucionado error de lectura de linea

--- acceso_a_datos/src/tarea9/Main.java
+++ acceso_a_datos/src/tarea9/Main.java
@@ -6,98 +6,73 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 public class Main {
-	
-	private Scanner prompt=new Scanner(System.in);
-	
+
+	private Scanner prompt = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Main().run();
 	}
+
+	private void run() {
+		menu();
+	}
 	
-	public void run() {
-		System.out.println("Ruta del fichero de subtitulos (.srt):");
-		String ruta="ejemplo.srt";//prompt.next();
+	
+	public void menu() {
+		int opcion=0;
 		
-		File archSrt=new File(ruta);
-		List<String> subtitulos=new ArrayList<String>();
+		do {
+		System.out.println("elige una opcion: 1-Generar xml de un archvio srt. 2-Generar xml de archivos .srt en carpeta");
+		opcion=prompt.nextInt();
+		prompt.nextLine();
 		
-		List<Atributo> atributos=null;
 		
-		try(BufferedReader leerArch=new BufferedReader(new FileReader(archSrt));) {
-			String contenidoSrt="";
+		switch (opcion) {
+		case 1: 
+			System.out.println("Ruta del fichero de subtitulos (.srt):");
+			String ruta = "ejemplo.srt";// prompt.next();
 			
+			//con el array r y b sacamos el nombre del .srt para ponerselo al xml en caso de que tenga una ruta mas extensa.
+			String r[]=ruta.split(".srt");
+			String b[]=r[0].split("/");
+			String nombre=b[b.length-1];
 			
-			String linea;
-			while((linea=leerArch.readLine())!=null) {
-				
-				
-					subtitulos.add(linea);
-				
-				contenidoSrt+=" "+linea;
-			}
-			leerArch.close();
-			System.out.println(contenidoSrt);
-			//String subtitulos[]=contenidoSrt.split("\\d{1}$");
+			TransformadorSRTaXML srt_a_Xml = new TransformadorSRTaXML(ruta,nombre,"subtitulos","1.0");
+			srt_a_Xml.crearXml();
 			
+			break;
+		case 2:
+			File dir=new File("D:\\empleo\\documentos Juan");
 			
-			ArchivoXml xml=new ArchivoXml("Subtitulos","subtitulos","1.0");
-			Document doc=xml.getDoc();
-			
-			
-			boolean siguienteSubtitulo=true;
-			
-			String texto="";
-			
-			for(String subtitulo:subtitulos) {
-				
-				if(subtitulo.matches("\\d+") && !siguienteSubtitulo) {//añadimos los datos de atributo y linea dentro de la etiqueta.
-					System.out.println("Añadidos los atributos a la etiqueta.");
-					System.out.println("Texto: "+texto);
-					xml.anadirAtributo(doc.getDocumentElement(), "subtitulo", texto, atributos);
-					siguienteSubtitulo=true;
+			if(dir.isDirectory()) {
+				String files[]=dir.list();
+				for(String file: files) {
+					File f=new File(file);
+					if(f.isFile()) {
+						
+						if(file.endsWith(".srt")) {
+							
+							System.out.println(file+"\n");
+						}
+					}
 				}
-				
-				  
-				if(subtitulo.matches("\\d+")) {//comprobamos que sea el numero de la columna.
-					System.out.println("Creacion de atributo columna:"+subtitulo+" \n");
-					atributos=new ArrayList<Atributo>();
-					atributos.add(new Atributo("numero", subtitulo));
-					
-					siguienteSubtitulo=false;
-					
-				}else if(subtitulo.contains("-->")) {//comprobamos si es el tiempo de inicio y fin
-					System.out.println("Creacion de atributos de tiempo. \n");
-					String tiempos[]=subtitulo.split("-->");
-					
-					atributos.add(new Atributo("inicio", tiempos[0].trim()));
-					atributos.add(new Atributo("fin", tiempos[1].trim()));
-				
-				}else if(!subtitulo.trim().equals(" ")) {//El texto que tendra la etiqueta.
-					System.out.println("Texto de la etiqueta con contenido:"+subtitulo+" \n");
-					texto+=" "+subtitulo;
-				}
-				
-				
-				
 			}
-			System.out.println("Fin bucle for.");
-			xml.crearXml();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			break;
+		default:
+			System.out.println("fin del programa");
 		}
 		
+		
+		}while(opcion!=3);
 	}
 
 }
--- acceso_a_datos/src/tarea9/TransformadorSRTaXML.java
+++ acceso_a_datos/src/tarea9/TransformadorSRTaXML.java
@@ -1,5 +1,130 @@
 package tarea9;
 
-public class TransformadorSRTaXML {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+
+public class TransformadorSRTaXML extends ArchivoXml {
+
+	private String srt,texto = "";;
+	private List<Atributo> atributos = null;
+	/**
+	 * @param srt
+	 */
+	public TransformadorSRTaXML(String srt, String nombre, String etiquetaRaiz, String version) {
+
+		super(nombre, etiquetaRaiz, version);
+		this.srt = srt;
+
+		this.transformador();
+	}
+
+	private void transformador() {
+		File archSrt = new File(this.getSrt());
+		List<Atributo> atributos = null;
+
+		try (BufferedReader leerArch = new BufferedReader(new FileReader(archSrt));) {
+			String linea;
+			
+			Document doc = this.getDoc();
+
+			boolean siguiente = true;
+
+			while ((linea = leerArch.readLine()) != null) {
+
+				if (linea.equals("") && !siguiente) {// añadimos los datos de atributo y linea dentro
+					// de la etiqueta.
+					//System.out.println("Añadidos los atributos a la etiqueta.");
+					//System.out.println("Texto: " + this.getTexto());
+
+					this.anadirAtributo(doc.getDocumentElement(), "subtitulo", this.getTexto(), this.getAtributos());
+					
+					this.setTexto("");
+					siguiente = true;
+				}
+
+				siguiente=siguienteSubtitulo(linea,siguiente);
+			}
+			leerArch.close();
+
+			System.out.println("Fin bucle.");
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("archivo no encontrado");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private boolean siguienteSubtitulo(String linea, boolean siguienteSubtitulo) {
+		if (linea.matches("\\d+")) {// comprobamos que sea el numero de la columna.
+			//System.out.println("Creacion de atributo columna:" + linea + " \n");
+
+			this.setAtributos(new ArrayList<Atributo>());
+
+			this.getAtributos().add(new Atributo("numero", linea));
+
+			siguienteSubtitulo = false;
+
+		} else if (linea.contains("-->")) {// comprobamos si es el tiempo de inicio y fin
+			//System.out.println("Creacion de atributos de tiempo. \n");
+			String tiempos[] = linea.split("-->");
+
+			this.getAtributos().add(new Atributo("inicio", tiempos[0].trim()));
+			this.getAtributos().add(new Atributo("fin", tiempos[1].trim()));
+			
+		} else if (!linea.equals("")) {// El texto que tendra la etiqueta.
+			//System.out.println("Texto de la etiqueta con contenido:" + linea + " \n");
+			this.setTexto(this.getTexto()+" "+linea);
+		}
+
+		return siguienteSubtitulo;
+	}
+
+	/**
+	 * @return the srt
+	 */
+	private String getSrt() {
+		return srt;
+	}
+
+	/**
+	 * @return the atributos
+	 */
+	private List<Atributo> getAtributos() {
+		return atributos;
+	}
+
+	/**
+	 * @param atributos the atributos to set
+	 */
+	private void setAtributos(List<Atributo> atributos) {
+		this.atributos = atributos;
+	}
+
+	/**
+	 * @return the texto
+	 */
+	private String getTexto() {
+		return texto;
+	}
+
+	/**
+	 * @param texto the texto to set
+	 */
+	private void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
 
 }
